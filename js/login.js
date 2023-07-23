fc4cd4c540bb45df4a29af819190e18d52bf1b52
Franco Giacobbe <frandg47@gmail.com>
2023-07-23 13:25:09 -0300

    creando validaciones desde js en login

--- js/login.js
+++ js/login.js
@@ -1,22 +1,62 @@
-const divLogin = document.getElementById("login");
-const divRegistro = document.getElementById("registro");
-const btnRegistrate = document.getElementById("btn-registro");
+const inputEmail = document.getElementById("correo");
+const inputContraseña = document.getElementById("password");
+const form = document.getElementById("formulario-login");
+const usuarioRechazado = document.getElementById("usuarioRechazado");
+const divIngreso = document.querySelector("#btn-ingreso");
+const divRegistro = document.querySelector("#btn-registro");
 
-const cambioARegistro = () => {
-    divLogin.classList.add("d-none");
-    divLogin.classList.remove("d-block");
-    divRegistro.classList.add("d-block");
-    divRegistro.classList.remove("d-none");
-}
+form.addEventListener("input", function () {
+    const valorCorreo = inputEmail.value.trim();
+    const valorContraseña = inputContraseña.value.trim();
+    const patronCorreo = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    const patronContraseña = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/;
+    inputContraseña.setAttribute("type", "password");
 
-const cambioALogin = () => {
-    divRegistro.classList.add("d-none");
-    divRegistro.classList.remove("d-block");
-    divLogin.classList.add("d-block");
-    divLogin.classList.remove("d-none");
-}
+    if (valorCorreo < 3 || valorCorreo > 30 || !patronCorreo.test(valorCorreo)) {
+        inputEmail.setCustomValidity(
+            "El valor ingresado debe de ser mayor a 3, menor a 30 y debe de contener un @ con una direccion de correo electronico"
+        );
+    } else {
+        inputEmail.setCustomValidity("");
+    }
+    if (
+        valorContraseña.length < 8 ||
+        valorContraseña.length > 16 ||
+        !patronContraseña.test(valorContraseña)) {
+        inputContraseña.setCustomValidity("Contraseña inválida");
+    } else {
+        inputContraseña.setCustomValidity("");
+    }
+});
 
-btnRegistrate.addEventListener("click", () => {
-    cambioARegistro();
-})
+let arrayUsuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+let arrayUsuario = [];
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let valorEmail = inputEmail.value;
+    let valorContraseña = inputContraseña.value;
+    let bandera = false;
+
+    let usuarioEncontrado = 0;
+
+    for (let i = 0; i < arrayUsuarios.length; i++) {
+        if (
+            valorEmail === arrayUsuarios[i].email &&
+            valorContraseña === arrayUsuarios[i].contraseña
+        ) {
+            usuarioEncontrado = arrayUsuarios[i];
+            bandera = true;
+            break;
+        }
+    }
+
+    if (bandera === true) {
+        arrayUsuario.push(usuarioEncontrado);
+        form.reset();
+        localStorage.setItem("login_success", JSON.stringify(arrayUsuario));
+        window.location.href = "../index.html";
+    } else {
+        usuarioRechazado.innerHTML = "<p>Contraseña incorrecta o usuario no encontrado</p>";
+    }
+});
