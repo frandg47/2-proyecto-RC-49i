b8fd3ee63c5efcdd339ed9050fa239c50eb7f851
Franco Giacobbe <frandg47@gmail.com>
2023-07-26 09:36:37 -0300

    direccionamiento de logout corregido

--- js/logout.js
+++ js/logout.js
@@ -7,14 +7,14 @@ const li_Administracion = document.getElementById("li-Administracion");
 Logout.addEventListener("click", () => {
     localStorage.removeItem("login_success");
     DropUsuario.classList.add("d-none");
-    btnIngreso.classList.remove("d-none"); 
+    btnIngreso.classList.remove("d-none");
     li_Administracion.classList.add("d-none");
 
     //redirijo usuario a index cuando sale
     const location = window.location.href;
-    if (location == "./acercaDenosotros.html") {
+    if (location == "./index.html") {
         window.location.href = "./login.html";
     } else {
-        window.location.href = "../index.html";
+        window.location.href = "../html/login.html";
     }
 });
