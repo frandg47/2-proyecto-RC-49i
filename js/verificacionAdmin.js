294a9b5d397f1cb8f341a10b8d112043748cf631
Franco Giacobbe <frandg47@gmail.com>
2023-07-26 22:32:49 -0300

    eliminando comentarios y clg

--- js/verificacionAdmin.js
+++ js/verificacionAdmin.js
@@ -2,9 +2,6 @@ const usuarios2 = JSON.parse(localStorage.getItem("usuarios"));
 const usuariosLogeado2 = JSON.parse(localStorage.getItem("login_success"));
 
 if (!usuariosLogeado2) {
-  console.log(
-    "Error: Local storage items 'usuarios' or 'login_success' not found."
-  );
   window.location.href = "../index.html";
 } else {
   const usuario = usuarios2[0];
